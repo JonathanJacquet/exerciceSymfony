371b9c54447a1ce27095faccc7fa07ccacc9571b
Jo <jonathan.jacquet62@laposte.net>
2019-01-29 12:01:44 +0100

    routing accueil, article, article +id, add article

---
+++ src/Controller/testController.php
@@ -0,0 +1,34 @@
+<?php
+// src/Controller/LuckyController.php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class testController extends AbstractController {
+     /**
+      * @Route("/accueil")
+      */
+      public function accueil ()  {
+      $welcome = "Bienvenue sur l'accueil du site";
+        return new Response(
+          '<html><body>'.$welcome.'</body></html>'
+        );
+      }
+      /**
+       * @Route("/article/{id}", name="app_single", requirements={"id"="\d+"})
+       */
+       public function article ($id = 1)  {
+         echo"Vous êtes sur la page concernant la référence de produit : ";
+         return $this->render("base.html.twig", ["id" => $id]);
+      }
+      /**
+       * @Route("/admin/article/add")
+       */
+       public function addArticle() {
+         echo "Vue pour ajouter un article";
+         return new Response();
+       }
+}
+?>
